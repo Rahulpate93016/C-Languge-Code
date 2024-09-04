27819db2ce3b7534d2c9eacf8cb57c0c2a6ca5c8
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2024-09-04 20:07:18 +0530

    Day 3 in C programming

---
+++ 3.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main(){
+    int a,b, sum;
+    printf("Enter the first Number ");
+    scanf("%d",&a);
+    
+
+    printf("Enter the second  Number ");
+    scanf("%d",&b);
+    
+     sum=a+b;
+    printf("The sum is:%d",sum);
+    return 0;
+
+     
+ }
