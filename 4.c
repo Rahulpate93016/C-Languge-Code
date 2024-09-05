b55e507a47940a222d6cbe1a76eaf09ffe5f05ff
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2024-09-05 22:06:13 +0530

    Day 4

---
+++ 4.c
@@ -0,0 +1,9 @@
+# include<stdio.h>   // madulo operater is return a remainder for int
+# include<math.h>
+int main(){
+    int a=40;
+    printf("the value of a is %d", a%6);
+    return 0;
+
+    
+}
