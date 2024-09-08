013238f7979de1555f589b39acab5921123e91e8
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2024-09-08 10:23:33 +0530

    Day 7

---
+++ 7.c
@@ -0,0 +1,28 @@
+/*# include<stdio.h>
+int main (){
+    int i=1;
+    while (i<10)
+    {
+        printf("%d \n",i);
+        i++;
+    }
+return 0;
+    
+}*/
+
+# include<stdio.h>
+ int main(){
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+     int i=0;
+    while (i<n)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+return 0;
+}
+
+
