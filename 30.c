5c4aae1a0ef8380e78ba014960aef086875fda3b
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2024-09-30 23:09:08 +0530

    Day 30

---
+++ 30.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+void swap(int a,int b);
+int main(){
+     int x=5;
+     int y=6;
+     
+     swap(x,y);
+      printf(" x=%d \n " " y=%d \n",x,y);
+    return 0;
+}
+//call value fubnction 
+void swap( int a, int b){
+    int t=a;
+    a=b;
+    b=t;
+    printf(" a= %d \n" " b=%d \n ",a,b);
+}
