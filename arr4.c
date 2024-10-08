f9a026e17914001c6ffe02c5922595596bda74c1
Rahul Patel <115304614+Rahulpate93016@users.noreply.github.com>
2024-10-08 09:36:50 +0530

    Array day 4 in c languge

---
+++ arr4.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+    int marks[2][3];
+    marks[0][0]=85;
+    marks[0][1]=85;
+    marks[0][2]=85;
+
+    marks[1][0]=85;
+    marks[1][1]=85;
+    marks[1][2]=85;
+    printf("%d",marks[1][1]);
+
+    return 0;
+}
